aeb492176c92b53c56d9ef40d1983c99a6dbf425
shamyratpashiyev96 <shamyratpashiyev96@gmail.com>
2025-07-08 18:37:53 +0500

    Initial commit

---
+++ MyDataStructuresAndAlgorithmsApp/SimpleDataStructuresExample.cs
@@ -0,0 +1,118 @@
+namespace MyDataStructuresAndAlgorithmsApp;
+
+public class SimpleDataStructuresExample
+{
+    public SimpleDataStructuresExample()
+    {
+        ArrayExample();
+        LinkedListExample();
+        StackExample();
+        QueueExample();
+    }
+    
+    public static void ArrayExample()
+    {
+        // Declare and initialize an array of calculator inputs
+        string[] inputHistory = { "5 + 3", "10 - 2", "7 * 4", "20 / 5", "3 ^ 2" };
+        
+        // Print the original input history
+        Console.WriteLine($"Original Calculator Input History:");
+        foreach (var input in inputHistory)
+        {
+            Console.WriteLine(input);
+        }
+        
+        // Modifying an entry by index (change "10 - 2" to "10 / 2"), because it is impossible to remove element from 
+        // fixed size array
+        inputHistory[1] = "10 / 2"; // Updating has O(1) time complexity
+        
+        // Print the updated input history
+        Console.WriteLine($"\n Updated Calculator Input History:");
+        foreach (var input in inputHistory)
+        {
+            Console.WriteLine(input);
+        }
+    }
+    
+    public static void LinkedListExample()
+    {
+        // Declaring and populating a LinkedList
+        var linkedList = new LinkedList<string>();
+        linkedList.AddLast(new LinkedListNode<string>("Node: 8"));
+        linkedList.AddLast(new LinkedListNode<string>("Node: 5"));
+        linkedList.AddLast(new LinkedListNode<string>("Node: 28"));
+        linkedList.AddLast(new LinkedListNode<string>("Node: 4"));
+        linkedList.AddLast(new LinkedListNode<string>("Node: 9"));
+        
+        // Looping through the linkedList
+        foreach (var node in linkedList)
+        {
+            Console.WriteLine(node);
+        }
+        
+        // Removing a node
+        linkedList.Remove("Node: 5");   //Removing has O(1) time complexity
+        
+        // Looping through updated linkedList
+        Console.WriteLine($"\n Looping through updated linkedList:");
+        foreach (var node in linkedList)
+        {
+            Console.WriteLine(node);
+        }
+    }
+    
+    public static void StackExample()
+    {
+        // Declaring and populating a stack
+        var undoHistory = new Stack<string>();
+        undoHistory.Push("Entered 5 + 3");
+        undoHistory.Push("Entered 10 - 2");
+        undoHistory.Push("Entered 7 * 4");
+        undoHistory.Push("Entered 20 / 5");
+        undoHistory.Push("Entered 3 ^ 2");
+        
+        // Looping through the stack
+        Console.WriteLine($"Undo History:");
+        foreach (var action in undoHistory)
+        {
+            Console.WriteLine(action);
+        }
+        
+        // Undoing an action by popping an element in a stack
+        Console.WriteLine($"\n Undoing an action: {undoHistory.Pop()}");
+        
+        Console.WriteLine($"\n Updated Undo History:");
+        // Printing the updated stack
+        foreach (var action in undoHistory)
+        {
+            Console.WriteLine(action);
+        }
+    }
+    
+    public static void QueueExample()
+    {
+        // Declaring and populating a queue
+        var pendingCalculations = new Queue<string>();
+        pendingCalculations.Enqueue("Calculate: 15 + 5");
+        pendingCalculations.Enqueue("Calculate: 12 - 3");
+        pendingCalculations.Enqueue("Calculate: 9 * 2");
+        pendingCalculations.Enqueue("Calculate: 16 / 4");
+        pendingCalculations.Enqueue("Calculate: 2 ^ 3");
+        
+        // Looping through the queue
+        foreach (var calculation in pendingCalculations)
+        {
+            Console.WriteLine(calculation);
+        }
+        
+        // Processing a calculation and dequeuing it
+        Console.WriteLine($"\n Processing a calculation: {pendingCalculations.Dequeue()}");
+        
+        // Printing the updated queue
+        Console.WriteLine($"\n Updated queue:");
+        foreach (var calculation in pendingCalculations)
+        {
+            Console.WriteLine(calculation);
+        }
+    }
+}
---
+++ MyDataStructuresAndAlgorithmsApp/SortingAlgorithmsExample.cs
@@ -0,0 +1,78 @@
+namespace MyDataStructuresAndAlgorithmsApp;
+
+public class SortingAlgorithmsExample
+{
+    public SortingAlgorithmsExample()
+    {
+        int[] randomArray = [47, 12, 85, 3, 91, 66, 25, 19, 76, 50];
+        // randomArray = BubbleSort(randomArray);
+        randomArray = QuickSort(randomArray);
+        foreach (var item in randomArray)
+        {
+            Console.WriteLine($"{item}");
+        }
+    }
+    
+    public static int[] BubbleSort(int[] arr)
+    {
+        if (arr.Length <= 1)
+        {
+            return arr;
+        }
+
+        var isSorted = false;
+        
+        while (!isSorted)
+        {
+            isSorted = true;
+            // arr.Length - 1 is used in order to avoid OutOfBoundaries exception
+            // (it sort of not iterates through the last element because it gets swapped anyway when the previous one is processed)
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] > arr[i + 1])
+                {
+                    (arr[i], arr[i + 1]) = (arr[i + 1], arr[i]);
+                    isSorted = false;
+                }
+            }
+        }
+        
+        return arr;
+    }
+    
+    public static int[] QuickSort(int[] arr)
+    {
+        if (arr.Length <= 1)
+        {
+            return arr;
+        }
+        
+        var lessThanArray = new int[0];
+        var greaterThanArray = new int[0];
+        
+        // Selecting and moving pivot to the first position
+        int pivotIndex = arr.Length / 2;
+        (arr[0], arr[pivotIndex]) = (arr[pivotIndex], arr[0]);
+        var pivot = arr[0];
+
+        // Omitting the first value since we moved pivot value there
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] <= pivot)
+            {
+                lessThanArray = lessThanArray.Append(arr[i]).ToArray();
+            }
+            else
+            {
+                greaterThanArray = greaterThanArray.Append(arr[i]).ToArray();
+            }
+        }
+        
+        return new int[0].Concat(QuickSort(lessThanArray)).Concat([pivot]).Concat(QuickSort(greaterThanArray)).ToArray();
+    }
+    
+    public static int[] MergeSort(int[] arr)
+    {
+        return arr;
+    }
+}
