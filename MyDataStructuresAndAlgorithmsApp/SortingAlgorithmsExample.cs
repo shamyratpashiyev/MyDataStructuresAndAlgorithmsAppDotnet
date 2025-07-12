7414f0782ed4509ef5d96aa50f0bfeba88bedf30
shamyratpashiyev96 <shamyratpashiyev96@gmail.com>
2025-07-12 18:45:43 +0500

    Implemented BinarySearch and added description to sorting algorithm implementations

--- MyDataStructuresAndAlgorithmsApp/SortingAlgorithmsExample.cs
+++ MyDataStructuresAndAlgorithmsApp/SortingAlgorithmsExample.cs
@@ -6,33 +6,40 @@ public class SortingAlgorithmsExample
 {
     public SortingAlgorithmsExample()
     {
-        // int[] randomArray = [47, 12, 85, 3, 91, 66, 25, 19, 76, 50];
-        var randomArray = GenerateRandomArray(100_000, 1, 100_000);
+        int[] randomArray = [47, 12, 85, 3, 91, 66, 25, 19, 76, 50];
+        // var randomArray = GenerateRandomArray(100_000, 1, 100_000);
+        //
+        // var timer = new Stopwatch();
+        // timer.Start();
+        // var builtInSortResult = randomArray.Order().ToArray();
+        // timer.Stop();
+        // Console.WriteLine($"builtInSortResult: {timer.ElapsedMilliseconds} ms, IsSorted: {IsSorted(builtInSortResult)}");
+        //
+        // timer.Restart();
+        // var mergeSortResult = MergeSort(randomArray);
+        // timer.Stop();
+        // Console.WriteLine($"mergeSortResult: {timer.ElapsedMilliseconds} ms, IsSorted: {IsSorted(mergeSortResult)}");
+        //
+        // timer.Restart();
+        // var quickSortResult = QuickSort(randomArray);
+        // timer.Stop();
+        // Console.WriteLine($"quickSortResult: {timer.ElapsedMilliseconds} ms, IsSorted: {IsSorted(quickSortResult)}");
+        //
+        // timer.Restart();
+        // var bubbleSortResult = BubbleSort(randomArray);
+        // timer.Stop();
+        // Console.WriteLine($"bubbleSortResult: {timer.ElapsedMilliseconds} ms, IsSorted: {IsSorted(bubbleSortResult)}");
         
-        var timer = new Stopwatch();
-        timer.Start();
-        var builtInSortResult = randomArray.Order().ToArray();
-        timer.Stop();
-        Console.WriteLine($"builtInSortResult: {timer.ElapsedMilliseconds} ms, IsSorted: {IsSorted(builtInSortResult)}");
-        
-        timer.Restart();
-        var mergeSortResult = MergeSort(randomArray);
-        timer.Stop();
-        Console.WriteLine($"mergeSortResult: {timer.ElapsedMilliseconds} ms, IsSorted: {IsSorted(mergeSortResult)}");
-
-        timer.Restart();
-        var quickSortResult = QuickSort(randomArray);
-        timer.Stop();
-        Console.WriteLine($"quickSortResult: {timer.ElapsedMilliseconds} ms, IsSorted: {IsSorted(quickSortResult)}");
-
-        timer.Restart();
-        var bubbleSortResult = BubbleSort(randomArray);
-        timer.Stop();
-        Console.WriteLine($"bubbleSortResult: {timer.ElapsedMilliseconds} ms, IsSorted: {IsSorted(bubbleSortResult)}");
+        Console.WriteLine($"Found: {BinarySearch(randomArray.Order().ToArray(), 92)}");
     }
     
     public static int[] BubbleSort(int[] arr)
     {
+        // The way it works is that it loops through the array comparing current elements value with the next one and swapping them
+        // if needed (sort of bubbles the least values up), until it is completely sorted. 
+        // It has in worst case O(n^2) time complexity
+        // It has O(1) space complexity meaning it does not use additional space since it operates directly on the array provided.
+        // It also preserves the original order of elements if they're equal.
         if (arr.Length <= 1)
         {
             return arr;
@@ -60,6 +67,10 @@ public class SortingAlgorithmsExample
     
     public static int[] QuickSort(int[] arr)
     {
+        // It randomly selects pivot value and moves elements to left or right array depending on their values and then merges them in the end.
+        // It has O(n log n) time complexity on average, but it can have O(n^2) in worst case, if the pivot is chosen poorly.
+        // It has O(log n) space complexity on average, but it can have O(n log n) in the worst case.
+        // It also does not preserve original position of the values that are equal
         if (arr.Length <= 1)
         {
             return arr;
@@ -91,6 +102,12 @@ public class SortingAlgorithmsExample
     
     public static int[] MergeSort(int[] arr)
     {
+        // The way it works is that it splits the array into two parts and iterates through both of them and compares the values.
+        // at the same index in both arrays on merging, and adds the least one to the merged array.
+        // It has O(n log n) time complexity always, no matter the length or ordering of the elements.
+        // It has O(n) space complexity because it creates a result array at each iteration.
+        // And it preserves the original position of the elements that are equal.
+
         // Stops the recursion at the right time
         if (arr.Length <= 1)
         {
@@ -130,6 +147,36 @@ public class SortingAlgorithmsExample
         return resultArray.Concat(leftArray[leftIndex..]).Concat(rightArray[rightIndex..]).ToArray();
     }
     
+    public static bool BinarySearch(int[] arr, int target)
+    {
+        // The way it works is that it just moves the startIndex and endIndex at each iteration until the element at middleIndex
+        // is the target value. It works this way for efficiency purposes (so it does not duplicate array at each iteration).
+        // It has O(log n) time complexity, but it works only if the array is sorted.
+        // It has O(1) space complexity because it operates directly on the original array.
+        var startIndex = 0;
+        var endIndex = arr.Length - 1;
+        var middleIndex = startIndex + (endIndex - startIndex) / 2;
+
+        while (startIndex <= endIndex)
+        {
+            if (arr[middleIndex] == target)
+            {
+                return true;
+            }
+            else if (target < arr[middleIndex])
+            {
+                endIndex = --middleIndex;
+            }
+            else if (target > arr[middleIndex])
+            {
+                startIndex = ++middleIndex;
+            }
+
+            middleIndex = startIndex + (endIndex - startIndex) / 2;
+        }
+        
+        return false;
+    }
     public static bool IsSorted(int[] arr)
     {
         var result = true;
